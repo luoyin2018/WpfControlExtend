48db2f74f8d75d21226bee062b8b946fe5a7b656
luoyin <luoyin2018@163.com>
2018-08-08 15:15:28 +0800

    添加项目文件。

---
+++ Roy.Wpf/FocusableElementHost.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Roy.Wpf
+{
+    public class FocusableElementHost:ContentControl
+    {
+        static FocusableElementHost()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FocusableElementHost),
+                new FrameworkPropertyMetadata(typeof(FocusableElementHost)));
+        }
+        public FocusableElementHost()
+        {
+            IsTabStop = false;
+            this.MouseDown += (s, e) => this.Focus();
+        }
+    }
+}
